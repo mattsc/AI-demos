ea55290e643d1683b471dec0ff8ba62d4e48f92c
mattsc <mschoeck@tmt.org>
2012-09-07 15:33:44 -0700

    Added some more comments

--- lua/eval_exec_CA.lua
+++ lua/eval_exec_CA.lua
@@ -7,6 +7,9 @@
 --    menu option to be set up in era.cfg, rather than for these functions here
 -- 2. The MP game needs to be launched from the MP lobby
 -- 3. The name of the CA to be tested needs to be set in function CA_name() below
+--
+-- Not required:
+-- 1. Reloading after making changes, either to this file, or to Fred's engine functions
 
 local function CA_name()
     -- This function sets the name of the CA to be executed or evaluated
@@ -37,6 +40,7 @@ return {
             end
         end
 
+        -- Now display the evaluation score
         if found then
             -- Need to set up a fake 'self.data' table, as that does not exist outside the AI
             if not my_ai.data then my_ai.data = {} end
