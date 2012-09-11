ddefd1e023e576dececa3b64c225a656bbdc2fc6
Alarantalara <alarantalara@gmail.com>
2012-09-10 22:18:35 -0400

    Calculate damage given flat terrain
    Multiply damage done by probabilities of hitting enemy on flat terrain.
    Include the effects of marksman and magical special abilities in
    calculation

--- lua/recruit_filter_helper.lua
+++ lua/recruit_filter_helper.lua
@@ -40,24 +40,43 @@ function analyze_enemy_unit(unit_type_id)
 
     local unit = wesnoth.create_unit { type = unit_type_id }
     local can_poison = not (helper.get_child(unit.__cfg, "status").not_living or wesnoth.unit_ability(unit, 'regenerate'))
+    local flat_defense = wesnoth.unit_defense(unit, "Gt")
     print(unit_type_id)
+
     for i, recruit_id in ipairs(wesnoth.sides[wesnoth.current.side].recruit) do
         local best_damage = 0
         local best_attack = nil
+
         for attack in helper.child_range(wesnoth.unit_types[recruit_id].__cfg, "attack") do
+            local defense = flat_defense
             local poison = false
-            -- TODO: handle more abilities (charge, steadfast, drain, marksman, magical)
+            -- TODO: handle more abilities (charge, steadfast, drain)
             for special in helper.child_range(attack, 'specials') do
+                local mod
                 if helper.get_child(special, 'poison') and can_poison then
                     poison = true
                 end
+
+                -- Handle marksman and magical
+                -- TODO: Make this work properly for UMC chance_to_hit
+                mod = helper.get_child(special, 'chance_to_hit')
+                if mod then
+                    if mod.cumulative then
+                        if mod.value > defense then
+                            defense = mod.value
+                        end
+                    else
+                        defense = mod.value
+                    end
+                end
             end
 
-            local attack_damage = attack.damage*attack.number*wesnoth.unit_resistance(unit, attack.type)
+            defense = defense/100.0
+            local attack_damage = attack.damage*attack.number*wesnoth.unit_resistance(unit, attack.type)*defense
 
-            -- include terrain defense (flat or other?)
-                attack_damage = attack_damage + 800
             if poison then
+                -- Add poison damage * probability of poisoning
+                attack_damage = attack_damage + 800*(1-((1-defense)^attack.number))
             end
 
             if attack_damage > best_damage then
