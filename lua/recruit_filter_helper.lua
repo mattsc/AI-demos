d3f43470d1d3c4a558552d9050d7d1ede2b40103
Alarantalara <alarantalara@gmail.com>
2012-09-10 21:04:02 -0400

    Basic calculation of recruit damage versus enemy unit

--- lua/recruit_filter_helper.lua
+++ lua/recruit_filter_helper.lua
@@ -1,4 +1,5 @@
 helper = wesnoth.require "lua/helper.lua"
+local DBG = wesnoth.require "~/add-ons/AI-demos/lua/debug.lua"
 
 function archer_target(unit)
     local fire_resist = wesnoth.unit_resistance(unit, "fire")
@@ -32,4 +33,24 @@ end
 
 function not_living(unit)
     return not (not helper.get_child(unit.__cfg, "status").not_living)
+end
+
+function analyze_enemy_unit(unit_type_id)
+    local analysis = {}
+
+    local unit = wesnoth.create_unit { type = unit_type_id }
+    for i, recruit_id in ipairs(wesnoth.sides[wesnoth.current.side].recruit) do
+        local best_damage = 0
+        local best_attack = nil
+        for attack in helper.child_range(wesnoth.unit_types[recruit_id].__cfg, "attack") do
+            local attack_damage = attack.damage*attack.number*wesnoth.unit_resistance(unit, attack.type)
+            -- TODO: include abilities (poison, charge, steadfast, drain, marksman, magical)
+            -- include terrain defense (flat or other?)
+            if attack_damage > best_damage then
+                best_damage = attack_damage
+                best_attack = attack
+            end
+        end
+        print(recruit_id, best_attack.name, best_damage)
+    end
 end
