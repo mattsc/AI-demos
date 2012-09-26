773750919747bd7cc74ac2691169279cf88246b5
mattsc <mschoeck@tmt.org>
2012-09-26 13:20:00 -0700

    Bottleneck Defense: cleaning up of attack CA functions

--- micro_ais/ais/mai_bottleneck_defense_engine.lua
+++ micro_ais/ais/mai_bottleneck_defense_engine.lua
@@ -541,29 +541,23 @@ return {
             -- This will be much easier once the 'attacks' variable is implemented
             local attackers = wesnoth.get_units {
                 side = wesnoth.current.side, formula = "$this_unit.attacks_left > 0",
-                { "filter_adjacent", { { "not", {side = wesnoth.current.side} } }
-                }
+                { "filter_adjacent", {
+                    { "filter_side", { { "enemy_of", {side = wesnoth.current.side} } } }
+                } }
             }
             --print("\n\nAttackers:",#attackers)
             if (not attackers[1]) then return 0 end
 
-            -- Variables to store best attacker/target pair
-            local max_rating = 0
-            local best_att = {}
-            local best_tar = {}
-            local best_weapon = -1
-
             -- Now loop through the attackers, and all attacks for each
+            local max_rating, best_att, best_tar, best_weapon = 0, {}, {}, -1
             for i,a in ipairs(attackers) do
-
                 local targets = wesnoth.get_units {
-                    { "not", {side = wesnoth.current.side} },
+                    { "filter_side", { {"enemy_of", {side = wesnoth.current.side} } } },
                     { "filter_adjacent", { id = a.id } }
                 }
                 --print("  ",a.id,#targets)
 
                 for j,t in ipairs(targets) do
-
                     local n_weapon = 0
                     for weapon in H.child_range(a.__cfg, "attack") do
                         n_weapon = n_weapon + 1
@@ -576,10 +570,10 @@ return {
                         -- 1. There is no counter attack
                         -- 2. Probability of death is >=67% for enemy, 0% for attacker
                         if (att_stats.hp_chance[a.hitpoints] == 1)
-                            or (def_stats.hp_chance[0] >= 0.67) and (att_stats.hp_chance[0] == 0)
+                            or ((def_stats.hp_chance[0] >= 0.67) and (att_stats.hp_chance[0] == 0))
                         then
                             rating = 1000 + t.max_hitpoints + def_stats.hp_chance[0]*100 + att_stats.average_hp - def_stats.average_hp
-                            -- if there's a chance to make the kill, unit closest to leveling up goes first, otherwise the other unit
+                            -- if there's a chance to make the kill, unit closest to leveling up goes first, otherwise the other way around
                             if (def_stats.hp_chance[0] >= 0.67) then
                                 rating = rating + (a.experience - a.max_experience) / 10.
                             else
@@ -588,15 +582,11 @@ return {
                         end
                         --print(a.id, t.id,weapon.name, rating)
                         if rating > max_rating then
-                            max_rating = rating
-                            best_att = a
-                            best_tar = t
-                            best_weapon = n_weapon
+                            max_rating, best_att, best_tar, best_weapon = rating, a, t, n_weapon
                         end
                     end
                 end
             end
-
             --print("Best attack:",best_att.id, best_tar.id, max_rating, best_weapon)
 
             if max_rating == 0 then
@@ -627,9 +617,7 @@ return {
                 ai.attack(self.data.attacker, self.data.target, self.data.weapon + dw)
             end
 
-            self.data.attacker = nil
-            self.data.target = nil
-            self.data.weapon = nil
+            self.data.attacker, self.data.target, self.data.weapon = nil, nil, nil
             self.data.bottleneck_attacks_done = nil
         end
 
