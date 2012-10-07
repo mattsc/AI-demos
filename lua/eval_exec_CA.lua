8b08300d57c80a4d17ae32d121aa05bee8d9fe84
mattsc <mschoeck@tmt.org>
2012-10-07 16:15:50 -0700

    Debug CA: separate functions for CA evaluation and execution

--- lua/eval_exec_CA.lua
+++ lua/eval_exec_CA.lua
@@ -37,15 +37,8 @@ local function get_all_CA_names()
     return cas
 end
 
-local function eval_exec_CA(exec_also, ai)
-    -- Evaluates and potentially executes the CA with name returned by CA_name()
-    -- Input:
-    --   exec_also = nil/false: only evaluate the CA
-    --   exec_also = true: also execute the CA, if eval score > 0
-    --   ai: the ai function table
-
-    local H = wesnoth.require "lua/helper.lua"
-    local W = H.set_wml_action_metatable {}
+local function eval_CA()
+    -- Evaluates the CA with name returned by CA_name()
 
     local eval_name = CA_name() .. '_eval'
     wesnoth.message("Evaluating individual CA: " .. eval_name .. "()")
@@ -68,44 +61,53 @@ local function eval_exec_CA(exec_also, ai)
         end
     end
 
-    -- Now display the evaluation score
+    -- Now display and return the evaluation score
     local score = 0
     if found then
         score = eval_function()
         wesnoth.message("  Score : " .. score)
     else
         wesnoth.message("  Found no function of that name")
-        return
-    end
-
-    -- If the score is positive and exec_also=true is set, execute the CA
-    if (score > 0) and exec_also then
-        if ai then
-            local exec_name = CA_name() .. '_exec'
-            wesnoth.message("  -> Executing individual CA: " .. exec_name .. "()")
-
-            -- Loop through the my_ai table until we find the function we are looking for
-            local exec_function = ''
-            for k,v in pairs(my_ai) do
-                if (k == exec_name) then
-                    exec_function = v
-                    exec_function()
-                end
-            end
-        else
-            wesnoth.message("!!!!! Error !!!!!  CAs not activated for execution.")
-        end
     end
 
     -- At the end, transfer my_ai.data content to global self_data_table
     self_data_table = my_ai.data
+
+    return score
+end
+
+local function exec_CA(ai)
+    -- Executes the CA with name returned by CA_name()
+
+    if ai then
+        local exec_name = CA_name() .. '_exec'
+        wesnoth.message("  -> Executing individual CA: " .. exec_name .. "()")
+
+        -- Get all the custom AI functions
+        my_ai = wesnoth.dofile("~add-ons/AI-demos/lua/grunt-rush-Freelands-S1_engine.lua").init(ai)
+
+        -- Need to set up a fake 'self.data' table, as that does not exist outside the AI engine
+        -- This is taken from the global table 'self_data_table', because it needs to persist between moves
+        my_ai.data = self_data_table
+
+        -- Loop through the my_ai table until we find the function we are looking for
+        local exec_function = ''
+        for k,v in pairs(my_ai) do
+            if (k == exec_name) then
+                exec_function = v
+                exec_function()
+            end
+        end
+    else
+        wesnoth.message("!!!!! Error !!!!!  CAs not activated for execution.")
+    end
 end
 
 return {
     debug_CA = function()
         -- CA debugging mode is enabled if this function returns true,
         -- that is, only if 'debug_CA_mode=true' is set and if we're in debug mode
-        local debug_CA_mode = false
+        local debug_CA_mode = true
         if debug_CA_mode and wesnoth.game_config.debug then
             wesnoth.fire_event("debug_CA")
         end
@@ -120,16 +122,22 @@ return {
 
         -- And call it for execution
         -- Don't need the actual 'ai_global' for that, but need a dummy table)
-        eval_exec_CA(true, {})
+        exec_CA({})
 
         -- Now reset the CA name
         wesnoth.set_variable('debug_CA_name', name)
     end,
 
-    eval_exec_CA = function(exec_also, ai)
+    eval_CA = function()
         -- This simply calls the function of the same name
         -- Done so that it is available from several functions inside this table
-        eval_exec_CA(exec_also, ai)
+        eval_CA()
+    end,
+
+    eval_exec_CA = function(ai)
+        -- This calls eval_CA(), then exec(CA) if the score is >0
+        local score = eval_CA()
+        if (score > 0) then exec_CA(ai) end
     end,
 
     choose_CA = function()
