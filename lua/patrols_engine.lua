da797145006f48cb714991ecc9026e6cf973c9c0
mattsc <mschoeck@tmt.org>
2012-11-11 19:17:06 -0800

    Patrols: clean up Jabb AI code

--- lua/patrols_engine.lua
+++ lua/patrols_engine.lua
@@ -92,53 +92,51 @@ return {
 
         function patrols:patrol_eval()
             -- acquire Jabb
-            local jabb = wesnoth.get_units( { id = "Goblin Handler Jabb" } )[1]
+            local jabb = wesnoth.get_units({ id = "Goblin Handler Jabb" })[1]
 
-            if (jabb.moves > 0) then return 300000 end
+            if jabb and (jabb.moves > 0) then return 300000 end
             return 0
         end
 
         function patrols:patrol_exec()
-                -- acquire Jabb
-                local jabb = wesnoth.get_units( { id = "Goblin Handler Jabb" } )[1]
+            -- acquire Jabb
+            local jabb = wesnoth.get_units( { id = "Goblin Handler Jabb" } )[1]
 
-                -- these are the waypoints
-                local steps = { {6,17},{7,14},{11,14},{11,18} }
-                -- this is where Jabb will move next
-                if not self.data.next_step_x then
-                        self.data.next_step_x = 7
-                        self.data.next_step_y = 14
+            -- these are the waypoints
+            local steps = { {6,17},{7,14},{11,14},{11,18} }
+
+            -- This is where Jabb will move next
+            if not self.data.next_step_x then
+                self.data.next_step_x = 7
+                self.data.next_step_y = 14
+            end
+
+            for index, location in ipairs( steps ) do
+                -- if Jabb is on a waypoint...
+                if jabb.x==location[1] and jabb.y==location[2] then
+                    if index >= #steps then
+                        -- ... move him to the first one, if he's on the last waypoint...
+                        self.data.next_step_x = steps[1][1]
+                        self.data.next_step_y = steps[1][2]
+                    else
+                        -- ... else move him on the next waypoint
+                        self.data.next_step_x = steps[index+1][1]
+                        self.data.next_step_y = steps[index+1][2]
+                    end
                 end
+            end
 
-                -- if he's dead, do nothing
-                if jabb and jabb.valid then
-                        for index, location in ipairs( steps ) do
-                                -- if Jabb is on a waypoint...
-                                if jabb.x==location[1] and jabb.y==location[2] then
-                                        if index >= #steps then
-                                                -- ... move him to the first one, if he's on the last waypoint...
-                                                self.data.next_step_x = steps[1][1]
-                                                self.data.next_step_y = steps[1][2]
-                                        else
-                                                -- ... else move him on the next waypoint
-                                                self.data.next_step_x = steps[index+1][1]
-                                                self.data.next_step_y = steps[index+1][2]
-                                        end
-                                end
-                        end
-
-                        -- perform the move
-                        local x, y = wesnoth.find_vacant_tile(self.data.next_step_x, self.data.next_step_y, jabb)
-                        local nh = AH.next_hop( jabb, x, y)
-                        AH.movefull_stopunit(ai, jabb, nh)
-                        -- at this point, Jabb should attack Jacques if adjacent
-                        local jacques = wesnoth.get_units( { id = "Jacques" } )[1]
-                        if jabb and jacques then
-                                if H.distance_between( jabb.x,jabb.y,jacques.x,jacques.y ) == 1 then --they're adjacent
-                                        ai.attack( jabb, jacques )
-                                end
-                        end
+            -- perform the move
+            local x, y = wesnoth.find_vacant_tile(self.data.next_step_x, self.data.next_step_y, jabb)
+            local nh = AH.next_hop( jabb, x, y)
+            AH.movefull_stopunit(ai, jabb, nh)
+            -- at this point, Jabb should attack Jacques if adjacent
+            local jacques = wesnoth.get_units( { id = "Jacques" } )[1]
+            if jabb and jacques then
+                if H.distance_between( jabb.x,jabb.y,jacques.x,jacques.y ) == 1 then --they're adjacent
+                    ai.attack( jabb, jacques )
                 end
+            end
         end
 
         return patrols
