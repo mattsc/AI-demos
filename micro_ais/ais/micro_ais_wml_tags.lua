bc8f7259ba473d5e3a260a6c3e66e5a22699af78
mattsc <mschoeck@tmt.org>
2012-11-14 16:16:24 -0800

    [micro_ai] tag definition: create a template section

--- micro_ais/ais/micro_ais_wml_tags.lua
+++ micro_ais/ais/micro_ais_wml_tags.lua
@@ -119,6 +119,29 @@ function wesnoth.wml_actions.micro_ai(cfg)
         return
     end
 
+    --------- Micro AI Template ------------------------------------
+    if (cfg.ai_type == 'template') then
+
+        -- Add the CAs
+        if (cfg.action == 'add') then
+            wesnoth.require "~add-ons/AI-demos/micro_ais/ais/template_CAs.lua".activate(cfg.side)
+        end
+
+        -- Change the CAs (done by deleting, then adding again, so that parameters get reset)
+        if (cfg.action == 'change') then
+            wesnoth.require "~add-ons/AI-demos/micro_ais/ais/template_CAs.lua".remove(cfg.side)
+            wesnoth.require "~add-ons/AI-demos/micro_ais/ais/template_CAs.lua".activate(cfg.side)
+        end
+
+        -- Remove the CAs
+        if (cfg.action == 'delete') then
+            wesnoth.require "~add-ons/AI-demos/micro_ais/ais/template_CAs.lua".remove(cfg.side)
+        end
+
+        return
+    end
+
+    ----------------------------------------------------------------
     -- If we got here, none of the valid ai_types was specified
     H.wml_error("invalid ai_type= in [micro_ai]")
 end
