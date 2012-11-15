6c7ab3cfeb0704132c71f26ef48b2ed5f8e0175d
mattsc <mschoeck@tmt.org>
2012-11-14 16:27:08 -0800

    Add Micro AI CA template file

---
+++ micro_ais/ais/template_CAs.lua
@@ -0,0 +1,45 @@
+return {
+    activate = function(side, cfg)
+        -- cfg contains extra options to be passed on to the CAs
+        -- This needs to be set up as a string
+
+        cfg = cfg or {}
+
+        -- Add the ??? keys (just an empty string for the template)
+        -- Can b left like this at first.  Modify when adding parameters.
+        local cfg_str = ''
+
+        local H = wesnoth.require "lua/helper.lua"
+        local W = H.set_wml_action_metatable {}
+
+        --print("Activating template for Side " .. side)
+
+        W.modify_ai {
+            side = side,
+            action = "add",
+            path = "stage[main_loop].candidate_action",
+            { "candidate_action", {
+                engine = "lua",
+                id = "template",
+                name = "template",
+                max_score = 300000,
+                evaluation = "return (...):template_eval(" .. cfg_str .. ")",
+                execution = "(...):template_exec()"
+            } }
+        }
+    end,
+
+    remove = function(side)
+
+        local H = wesnoth.require "lua/helper.lua"
+        local W = H.set_wml_action_metatable {}
+
+        --print("Removing template for Side " .. side)
+
+        W.modify_ai {
+            side = side,
+            action = "try_delete",
+            path = "stage[main_loop].candidate_action[template]"
+        }
+    end
+}
