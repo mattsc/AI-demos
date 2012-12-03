9c03b2bd392d2c786afabebb577badb9a4b096ba
mattsc <mschoeck@tmt.org>
2012-12-02 22:14:34 -0800

    micro_ais_wml_tags.lua: remove template
    There are so many examples now, it's not needed any more

--- micro_ais/ais/micro_ais_wml_tags.lua
+++ micro_ais/ais/micro_ais_wml_tags.lua
@@ -493,31 +493,6 @@ function wesnoth.wml_actions.micro_ai(cfg)
         return
     end
 
-    --------- Micro AI Template ------------------------------------
-    if (cfg.ai_type == 'template') then
-
-         -- Set up the cfg array
-        local cfg_template = {}
-
-        -- Add the CAs
-        if (cfg.action == 'add') then
-            wesnoth.require "~add-ons/AI-demos/micro_ais/ais/template_CAs.lua".add(cfg.side, cfg_template)
-        end
-
-        -- Change the CAs (done by deleting, then adding again, so that parameters get reset)
-        if (cfg.action == 'change') then
-            wesnoth.require "~add-ons/AI-demos/micro_ais/ais/template_CAs.lua".delete(cfg.side)
-            wesnoth.require "~add-ons/AI-demos/micro_ais/ais/template_CAs.lua".add(cfg.side, cfg_template)
-        end
-
-        -- Delete the CAs
-        if (cfg.action == 'delete') then
-            wesnoth.require "~add-ons/AI-demos/micro_ais/ais/template_CAs.lua".delete(cfg.side)
-        end
-
-        return
-    end
-
     ----------------------------------------------------------------
     -- If we got here, none of the valid ai_types was specified
     H.wml_error("invalid ai_type= in [micro_ai]")
