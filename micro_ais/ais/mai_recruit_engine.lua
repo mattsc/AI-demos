2703db826e8e9cc7b4374f2f91f8e06d291dd421
Martin Bede <bmartin9x@gmail.com>
2012-12-08 16:42:07 +0100

    Fixed whitespace.

--- micro_ais/ais/mai_recruit_engine.lua
+++ micro_ais/ais/mai_recruit_engine.lua
@@ -16,52 +16,53 @@ return {
                 return 0
             end
 
-	    -- Check if there is no space left for recruiting
-	    local width,height,border = wesnoth.get_map_size()
-            local castle = {
-                    locs = wesnoth.get_locations {
-                        x = "1-"..width, y = "1-"..height,
-                        { "and", {
-                            x = leader.x, y = leader.y, radius = 200,
-                            { "filter_radius", { terrain = 'C*^*,K*^*,*^Kov,*^Cov' } }
-                        }}
-                    },
-                    x = leader.x,
-                    y = leader.y
-                }
-            local no_space = true
-            for i,c in ipairs(castle.locs) do
-                local unit = wesnoth.get_unit(c[1], c[2])
-                if (not unit) then
-                    no_space = false
-                    break
+        -- Check if there is no space left for recruiting
+        local width,height,border = wesnoth.get_map_size()
+        local castle = {
+            locs = wesnoth.get_locations {
+                x = "1-"..width, y = "1-"..height,
+                { "and", {
+                    x = leader.x, y = leader.y, radius = 200,
+                    { "filter_radius", { terrain = 'C*^*,K*^*,*^Kov,*^Cov' } }
+                }}
+            },
+            x = leader.x,
+            y = leader.y
+        }
+        local no_space = true
+        for i,c in ipairs(castle.locs) do
+            local unit = wesnoth.get_unit(c[1], c[2])
+            if (not unit) then
+                no_space = false
+                break
+            end
+        end
+        if no_space then
+            return 0
+        end
+            
+        local possible_recruits = wesnoth.sides[wesnoth.current.side].recruit
+        recruit = possible_recruits[math.random(#possible_recruits)]
+        if low_gold_recruit == "random" then
+            if wesnoth.unit_types[recruit].cost <= wesnoth.sides[wesnoth.current.side].gold then
+                return 180000
+            end
+        end
+        if low_gold_recruit == "affordable" then
+            while #possible_recruits > 0 do
+                local i = 1
+                if #possible_recruits > 1 then
+                    i = math.random(#possible_recruits)
+                end
+                recruit = possible_recruits[i]
+                table.remove(possible_recruits, i)
+                if wesnoth.unit_types[recruit].cost <= wesnoth.sides[wesnoth.current.side].gold then
+                    return 180000
                 end
             end
-            if no_space then
-                return 0
-            end
-            
-            local possible_recruits = wesnoth.sides[wesnoth.current.side].recruit
-            recruit = possible_recruits[math.random(#possible_recruits)]
-	    if low_gold_recruit == "random" then
-		    if wesnoth.unit_types[recruit].cost <= wesnoth.sides[wesnoth.current.side].gold then
-		        return 180000
-		    end
-	    end
-	    if low_gold_recruit == "affordable" then
-	    	while #possible_recruits > 0 do
-			local i = 1
-			if #possible_recruits > 1 then
-				i = math.random(#possible_recruits)
-			end
-			recruit = possible_recruits[i]
-			table.remove(possible_recruits, i)
-			if wesnoth.unit_types[recruit].cost <= wesnoth.sides[wesnoth.current.side].gold then
-		        	return 180000
-			end
-		end
-	     end
-	   return 0
+        end
+        
+        return 0
         end
         
         function recruit_cas:random_recruit_exec()
