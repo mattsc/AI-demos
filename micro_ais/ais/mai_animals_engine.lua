1a6593fe153ba20f89935dfef118f19cf8eb3888
mattsc <mschoeck@tmt.org>
2012-12-22 15:26:50 -0800

    Wolves AI: clean up code

--- micro_ais/ais/mai_animals_engine.lua
+++ micro_ais/ais/mai_animals_engine.lua
@@ -228,20 +228,18 @@ return {
                 { "filter_side", {{"enemy_of", {side = wesnoth.current.side} }} }
             }
             --print('#avoid_units', #avoid_units)
-            -- negative hit for hexes the bears, spiders and yetis can attack
+            -- negative hit for hexes these types of units can attack
             local avoid = BC.get_attack_map(avoid_units).units  -- max_moves=true is always set for enemy units
 
             -- Find prey that is closest to all 3 wolves
-            local target = {}
-            local min_dist = 9999
+            local target, min_dist = {}, 9999
             for i,p in ipairs(prey) do
                 local dist = 0
                 for j,w in ipairs(wolves) do
                     dist = dist + H.distance_between(w.x, w.y, p.x, p.y)
                 end
                 if (dist < min_dist) then
-                    min_dist = dist
-                    target = p
+                    min_dist, target = dist, p
                 end
             end
             --print('target:', target.x, target.y, target.id)
@@ -261,7 +259,7 @@ return {
                 if (w - x) <= 5 then rating = rating - (6 - (w - x)) / 1.4 end
                 if (h - y) <= 5 then rating = rating - (6 - (h - y)) / 1.4 end
 
-               -- Hexes that enemy bears, yetis and spiders can reach get a massive negative hit
+               -- Hexes that avoid_type units can reach get a massive negative hit
                -- meaning that they will only ever be chosen if there's no way around them
                if avoid:get(x, y) then rating = rating - 1000 end
 
@@ -275,18 +273,19 @@ return {
                 move = AH.find_best_move(wolves[i], function(x,y)
                     local rating = 0
 
-                    -- We want wolves to be 2 hexes from each other
+                    -- We ideally want wolves to be 2-3 hexes from each other
+                    -- but this requirement gets weaker and weaker with increasing wolf number
                     for j = 1,i-1 do
                         local dst = H.distance_between(x, y, wolves[j].x, wolves[j].y)
-                        rating = rating + (-1 * (dst - 2.7 * j)^2 / j)
+                        rating = rating - (dst - 2.7 * j)^2 / j
                     end
 
                     -- Same distance from Wolf 1 and target for all the wolves
                     local dst_t = H.distance_between(x, y, target.x, target.y)
                     local dst_1t = H.distance_between(wolf1[1], wolf1[2], target.x, target.y)
-                    rating = rating + (-1 * (dst_t - dst_1t)^2)
+                    rating = rating - (dst_t - dst_1t)^2
 
-                    -- Hexes that enemy bears, yetis and spiders can reach get a massive negative hit
+                    -- Hexes that avoid_type units can reach get a massive negative hit
                     -- meaning that they will only ever be chosen if there's no way around them
                     if avoid:get(x, y) then rating = rating - 1000 end
 
@@ -299,7 +298,10 @@ return {
 
         function animals:wolves_wander_eval(cfg)
             -- When there's no prey left, the wolves wander and regroup
-            local wolves = wesnoth.get_units { side = wesnoth.current.side, formula = '$this_unit.moves > 0', { "and", cfg.predators } }
+            local wolves = wesnoth.get_units { side = wesnoth.current.side,
+                formula = '$this_unit.moves > 0', { "and", cfg.predators }
+            }
+
             if wolves[1] then
                 return 90000
             else
@@ -308,8 +310,9 @@ return {
         end
 
         function animals:wolves_wander_exec(cfg)
-
-            local wolves = wesnoth.get_units { side = wesnoth.current.side, formula = '$this_unit.moves > 0', { "and", cfg.predators } }
+            local wolves = wesnoth.get_units { side = wesnoth.current.side,
+                formula = '$this_unit.moves > 0', { "and", cfg.predators }
+            }
 
             -- Number of wolves that can reach each hex
             local reach_map = LS.create()
@@ -318,24 +321,21 @@ return {
                 reach_map:union_merge(r, function(x, y, v1, v2) return (v1 or 0) + (v2 or 0) end)
             end
 
-            -- Add a random rating; avoid big animals, tuskers
-            -- On their wandering, they avoid the dogs (but not when attacking)
+            -- Add a random rating; avoid avoid_type units
             local avoid_units = wesnoth.get_units { type = cfg.avoid_type,
                 { "filter_side", {{"enemy_of", {side = wesnoth.current.side} }} }
             }
             --print('#avoid_units', #avoid_units)
-            -- negative hit for hexes the bears, spiders and yetis can attack
+            -- negative hit for hexes these units can attack
             local avoid = BC.get_attack_map(avoid_units).units
 
-            local max_rating = -9e99
-            local goal_hex = {}
+            local max_rating, goal_hex = -9e99, {}
             reach_map:iter( function (x, y, v)
                 local rating = v + AH.random(99)/100.
                 if avoid:get(x, y) then rating = rating - 1000 end
 
                 if (rating > max_rating) then
-                    max_rating = rating
-                    goal_hex = { x, y }
+                    max_rating, goal_hex = rating, { x, y }
                 end
 
                 reach_map:insert(x, y, rating)
@@ -345,7 +345,6 @@ return {
 
             for i,w in ipairs(wolves) do
                 -- For each wolf, we need to check that goal hex is reachable, and out of harm's way
-
                 local best_hex = AH.find_best_move(w, function(x, y)
                     local rating = - H.distance_between(x, y, goal_hex[1], goal_hex[2])
                     if avoid:get(x, y) then rating = rating - 1000 end
