f8a3086457f9248825ba150f2d84d6f2cbdbb1a5
mattsc <mschoeck@tmt.org>
2013-04-16 10:56:32 -0700

    Goto Micro AI: add release_unit_at_goal functionality

--- micro_ais/ais/mai_goto_engine.lua
+++ micro_ais/ais/mai_goto_engine.lua
@@ -13,8 +13,17 @@ return {
                 { "and", cfg.goto_units }, formula = '$this_unit.moves > 0'
             }
 
-            local score = cfg.ca_score or 210000
+            -- Exclude released units
+            if cfg.release_unit_at_goal then
+                for i=#units,1,-1 do
+                    local str = cfg.ca_id .. '-release-' .. units[i].id
+                    if self.data[str] then
+                        table.remove(units, i)
+                    end
+                end
+            end
 
+            local score = cfg.ca_score or 210000
             if units[1] then return score end
             return 0
         end
@@ -23,6 +32,16 @@ return {
             local units = wesnoth.get_units { side = wesnoth.current.side, canrecruit = "no",
                 { "and", cfg.goto_units }, formula = '$this_unit.moves > 0'
             }
+
+            -- Exclude released units
+            if cfg.release_unit_at_goal then
+                for i=#units,1,-1 do
+                    local str = cfg.ca_id .. '-release-' .. units[i].id
+                    if self.data[str] then
+                        table.remove(units, i)
+                    end
+                end
+            end
             --print('#units', #units)
 
             -- Find all locations matching [goto_goals], excluding border hexes
@@ -78,6 +97,29 @@ return {
             --print(best_unit.id, best_unit.x, best_unit.y, closest_hex[1], closest_hex[2], max_rating)
 
             AH.movefull_stopunit(ai, best_unit, closest_hex[1], closest_hex[2])
+
+            -- If release_unit_at_goal= or release_all_units_at_goal= key is set:
+            -- Check if the unit made it to one of the goal hexes
+            -- This needs to be done for the original goal hexes, not checking the SLF again,
+            -- as that might have changed based on the new situation on the map
+            if cfg.release_unit_at_goal or cfg.release_all_units_at_goal then
+                local unit_at_goal = false
+                for i,l in ipairs(locs) do
+                    if (best_unit.x == l[1]) and (best_unit.y == l[2]) then
+                        unit_at_goal = true
+                        break
+                    end
+                end
+
+                -- If a unit was found, mark either it or all units as released
+                if unit_at_goal then
+                    if cfg.release_unit_at_goal then
+                        local str = cfg.ca_id .. '-release-' .. best_unit.id
+                        --print("Made it to goal: ", best_unit.id, str)
+                        self.data[str] = true
+                    end
+                end
+            end
         end
 
         return goto_engine
