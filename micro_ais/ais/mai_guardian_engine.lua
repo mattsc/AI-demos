47f25cecd74aa5eea927622d15d0e5c695f84c71
Zazweda <valentin.deschaintre@gmail.com>
2013-04-22 23:05:09 +0200

    Fixing indentation

--- micro_ais/ais/mai_guardian_engine.lua
+++ micro_ais/ais/mai_guardian_engine.lua
@@ -135,139 +135,140 @@ return {
 	--Used required params : id, zone_goals (SFL)
 	--Used optionnal param : zone_enemy (SLF)
 
-	-- The zone guardian will randomly move accross the specified zone_goals until it "detects" an enemy in the zone_enemy(if specified) or in the zone_goals(otherwise). It will then attack this enemy until it goes out of the "attack zone"
+	-- The zone guardian will randomly move accross the specified zone_goals until it "detects" an enemy in the zone_enemy(if specified) or in the zone_goals 				(otherwise). It will then attack this enemy until it goes out of the "attack zone"
 
-        function guardians:zone_guardian_eval(cfg)
-            local unit = wesnoth.get_units { id = cfg.id }[1]
+		function guardians:zone_guardian_eval(cfg)
+		    local unit = wesnoth.get_units { id = cfg.id }[1]
 
-            -- Don't need to check if unit exists as this is a sticky CA
-            if (unit.moves	 > 0) then
-                return 100010
-            else
-                return 99990
-            end
-        end
+		    -- Don't need to check if unit exists as this is a sticky CA
+		    if (unit.moves	 > 0) then
+		        return 100010
+		    else
+		        return 99990
+		    end
+		end
 	
-	--Moves the guardian randomly in the "zone_goals".
-        function guardians:zone_guardian_move_randomly(cfg, unit, ai)
-            local width, height = wesnoth.get_map_size()
-	    local locs = wesnoth.get_locations { 
-		        x = '1-' .. width,
-		        y = '1-' .. height,
-		        { "and", cfg.zone_goals }
-		}
-		local old_rand_val = 0
-		local newpos
-                for i,l in ipairs(locs) do
-			local randVal = math.random(100)
-			if randVal > old_rand_val then
-				newpos = i
-				old_rand_val = randVal			
+		--Moves the guardian randomly in the "zone_goals".
+		function guardians:zone_guardian_move_randomly(cfg, unit, ai)
+		    local width, height = wesnoth.get_map_size()
+			local locs = wesnoth.get_locations { 
+			    x = '1-' .. width,
+			    y = '1-' .. height,
+			    { "and", cfg.zone_goals }
+			}
+			local old_rand_val = 0
+			local newpos
+			for i,l in ipairs(locs) do
+				local randVal = math.random(100)
+				if randVal > old_rand_val then
+					newpos = i
+					old_rand_val = randVal			
+				end
+			end
+	
+			local nh = AH.next_hop(unit, locs[newpos][1], locs[newpos][2])
+			if nh then 
+		        AH.movefull_stopunit(ai, unit, nh)
 			end
 		end
+
+		--Check if an enemy is detected in the zone_enemy (or zone_goals) and attack it or start the "move" randomly function
+		function guardians:zone_guardian_exec(cfg)
+	 	    local unit = wesnoth.get_units{ id = cfg.id }[1]
+		    local reach = wesnoth.find_reach(unit)
+		    -- enemy units within reach
+
+			local enemies
+			if (cfg.zone_enemy) then
+				enemies = wesnoth.get_units {
+					{ "filter_side", {{"enemy_of", {side = wesnoth.current.side} }} },
+					{ "and", cfg.zone_enemy }
+				}
+			else
+				enemies = wesnoth.get_units {
+					{ "filter_side", {{"enemy_of", {side = wesnoth.current.side} }} },
+					{ "and", cfg.zone_goals }
+				}
+			end
 	
-		local nh = AH.next_hop(unit, locs[newpos][1], locs[newpos][2])
-		if nh then 
-	                AH.movefull_stopunit(ai, unit, nh)
-		end
-	end
-	--Check if an enemy is detected in the zone_enemy (or zone_goals) and attack it or start the "move" randomly function
-        function guardians:zone_guardian_exec(cfg)
- 	    local unit = wesnoth.get_units{ id = cfg.id }[1]
-            local reach = wesnoth.find_reach(unit)
-            -- enemy units within reach
+		    if enemies[1] then
 
-	    local enemies
-		if (cfg.zone_enemy) then
-			enemies = wesnoth.get_units {
-				{ "filter_side", {{"enemy_of", {side = wesnoth.current.side} }} },
-				{ "and", cfg.zone_enemy }
-			}
-		else
-			enemies = wesnoth.get_units {
-				{ "filter_side", {{"enemy_of", {side = wesnoth.current.side} }} },
-				{ "and", cfg.zone_goals }
-			}
-		end
-		
-            if enemies[1] then
-
-		    local target = {}
-		    local min_dist = 9999
-		    for i,e in ipairs(enemies) do
+			local target = {}
+			local min_dist = 9999
+			for i,e in ipairs(enemies) do
 			local dg = H.distance_between(unit.x, unit.y, e.x, e.y)
 
-		        -- If valid target found, save the one with the shortest distance from unit
-		        if (dg < min_dist) then
-		            --print("target:", e.id, ds, dg)
-		            target = e
-		            min_dist = dg
-		        end
-		    end
+			    -- If valid target found, save the one with the shortest distance from unit
+			    if (dg < min_dist) then
+			        --print("target:", e.id, ds, dg)
+			        target = e
+			        min_dist = dg
+			    end
+			end
 
-	-- If a valid target was found, unit attacks this target, or moves toward it
-		    if (min_dist ~= 9999) then
-		        --print ("Go for enemy unit:", target.id)
+		-- If a valid target was found, unit attacks this target, or moves toward it
+			if (min_dist ~= 9999) then
+			    --print ("Go for enemy unit:", target.id)
 
-		        -- Find tiles adjacent to the target, and save the one that our unit
-		        -- can reach with the highest defense rating
-		        local best_defense, attack_loc = -9e99, {}
-		        for x,y in H.adjacent_tiles(target.x, target.y) do
-		            -- only consider unoccupied hexes
-		            local occ_hex = wesnoth.get_units { x=x, y=y, { "not", { id = unit.id } } }[1]
-		            if not occ_hex then
-		                -- defense rating of the hex
-		                local defense = 100 - wesnoth.unit_defense(unit, wesnoth.get_terrain(x, y))
-		                --print(x,y,defense)
-		                local nh = AH.next_hop(unit, x, y)
-		                -- if this is best defense rating and unit can reach it, save this location
-				if nh then
-				        if (nh[1] == x) and (nh[2] == y) and (defense > best_defense) then
-				            best_defense, attack_loc = defense, {x, y}
-				        end
+			    -- Find tiles adjacent to the target, and save the one that our unit
+			    -- can reach with the highest defense rating
+			    local best_defense, attack_loc = -9e99, {}
+			    for x,y in H.adjacent_tiles(target.x, target.y) do
+			        -- only consider unoccupied hexes
+			        local occ_hex = wesnoth.get_units { x=x, y=y, { "not", { id = unit.id } } }[1]
+			        if not occ_hex then
+			            -- defense rating of the hex
+			            local defense = 100 - wesnoth.unit_defense(unit, wesnoth.get_terrain(x, y))
+			            --print(x,y,defense)
+			            local nh = AH.next_hop(unit, x, y)
+			            -- if this is best defense rating and unit can reach it, save this location
+						if nh then
+					   		if (nh[1] == x) and (nh[2] == y) and (defense > best_defense) then
+					    	    best_defense, attack_loc = defense, {x, y}
+					    	end
+						end
+				    end
 				end
-		            end
-		        end
 
-		        -- If a valid hex was found: move there and attack
-		        if (best_defense ~= -9e99) then
-		            --print("Attack at:",attack_loc[1],attack_loc[2],best_defense)
-		            AH.movefull_stopunit(ai, unit, attack_loc)
-		            ai.attack(unit, target)
-		        else  -- otherwise move toward that enemy
-		            --print("Cannot reach target, moving toward it")
-		            local reach = wesnoth.find_reach(unit)
+			    -- If a valid hex was found: move there and attack
+			    if (best_defense ~= -9e99) then
+			        --print("Attack at:",attack_loc[1],attack_loc[2],best_defense)
+			        AH.movefull_stopunit(ai, unit, attack_loc)
+			        ai.attack(unit, target)
+			    else  -- otherwise move toward that enemy
+			        --print("Cannot reach target, moving toward it")
+			        local reach = wesnoth.find_reach(unit)
 
-		            -- Go through all hexes the unit can reach, find closest to target
-		            local nh = {}  -- cannot use next_hop here since target hex is occupied by enemy
-		            local min_dist = 9999
-		            for i,r in ipairs(reach) do
-		                -- only consider unoccupied hexes
-		                local occ_hex = wesnoth.get_units { x=r[1], y=r[2], { "not", { id = unit.id } } }[1]
-		                if not occ_hex then
-		                    local d = H.distance_between(r[1], r[2], target.x, target.y)
-		                    if d < min_dist then
-		                        min_dist = d
-		                        nh = {r[1], r[2]}
-		                    end
-		                end
-		            end
+			        -- Go through all hexes the unit can reach, find closest to target
+			        local nh = {}  -- cannot use next_hop here since target hex is occupied by enemy
+			        local min_dist = 9999
+			        for i,r in ipairs(reach) do
+			            -- only consider unoccupied hexes
+			            local occ_hex = wesnoth.get_units { x=r[1], y=r[2], { "not", { id = unit.id } } }[1]
+			            if not occ_hex then
+			                local d = H.distance_between(r[1], r[2], target.x, target.y)
+			                if d < min_dist then
+			                    min_dist = d
+			                    nh = {r[1], r[2]}
+			                end
+			            end
+			        end
 
-		            -- Finally, execute the move toward the target
-		            AH.movefull_stopunit(ai, unit, nh)
-		        end
+				        -- Finally, execute the move toward the target
+				        AH.movefull_stopunit(ai, unit, nh)
+				end
+			end
+			-- If no enemy around or within the zone, move toward "random" position which are mainy the borders
+			else
+				--print "Move toward newpos"
+				guardians:zone_guardian_move_randomly(cfg, unit, ai)
+			end
+
+		    -- Get unit again, just in case something was done to it in a 'moveto' or 'attack' event
+		    local unit = wesnoth.get_units{ id = cfg.id }[1]
+		    if unit then ai.stopunit_moves(unit) end
+		    -- If there are attacks left and unit ended up next to an enemy, we'll leave this to RCA AI
 		end
-            -- If no enemy around or within the zone, move toward "random" position which are mainy the borders
-            else
-                --print "Move toward newpos"
-		guardians:zone_guardian_move_randomly(cfg, unit, ai)
-            end
-
-            -- Get unit again, just in case something was done to it in a 'moveto' or 'attack' event
-            local unit = wesnoth.get_units{ id = cfg.id }[1]
-            if unit then ai.stopunit_moves(unit) end
-            -- If there are attacks left and unit ended up next to an enemy, we'll leave this to RCA AI
-        end
 
 
 
