ea6f22bfd74f1399936bb3404a8a2de704cc1a48
mattsc <mattsc000@gmail.com>
2017-11-13 07:12:23 -0800

    Fred AI config: minor code clean-up

--- AI-demos/lua/fred_config.lua
+++ AI-demos/lua/fred_config.lua
@@ -3,21 +3,23 @@
 local fred_config = {}
 
 function fred_config.get_cfg_parm(parm)
-    local cfg = {
-        value_ratio = 0.8,  -- how valuable are own units compared to enemies
+    -- Note: accessing these parameters is comparatively slow and should not be done over and over
 
-        leader_weight = 1.5,
+    local cfg = {
+        value_ratio = 0.8,  -- value of own units compared to enemies
+
+
+        ----- Do not change values below unless you know exactly what you are doing -----
 
         xp_weight = 1.0,
+        leader_weight = 1.5,
+        leader_derating = 0.5,
+
+        villages_per_unit = 2,
 
         terrain_defense_weight = 0.1,
         distance_leader_weight = 0.002,
         occupied_hex_penalty = 0.001,
-
-        villages_per_unit = 2,
-
-        leader_derating = 0.5,
-
         use_max_damage_weapons = false
     }
 
