fda934293d7a0c10f3fa9d6f1ef742fe5420af3a
mattsc <mattsc000@gmail.com>
2017-11-13 07:52:11 -0800

    Fred AI config: pull the parameter table out of the function
    For speed reasons.  See comments in code.

--- AI-demos/lua/fred_config.lua
+++ AI-demos/lua/fred_config.lua
@@ -1,28 +1,30 @@
 -- AI configuration parameters
 
+-- Note: Assigning this table is slow compared to accessing values in it. It is
+-- thus done outside the functions below, so that it is not done over and over
+-- for each function call.
+
+local cfg = {
+    value_ratio = 0.8,  -- value of own units compared to enemies
+
+
+    ----- Do not change values below unless you know exactly what you are doing -----
+
+    xp_weight = 1.0,
+    leader_weight = 1.5,
+    leader_derating = 0.5,
+
+    villages_per_unit = 2,
+
+    terrain_defense_weight = 0.1,
+    distance_leader_weight = 0.002,
+    occupied_hex_penalty = 0.001,
+    use_max_damage_weapons = false
+}
+
 local fred_config = {}
 
 function fred_config.get_cfg_parm(parm)
-    -- Note: accessing these parameters is comparatively slow and should not be done over and over
-
-    local cfg = {
-        value_ratio = 0.8,  -- value of own units compared to enemies
-
-
-        ----- Do not change values below unless you know exactly what you are doing -----
-
-        xp_weight = 1.0,
-        leader_weight = 1.5,
-        leader_derating = 0.5,
-
-        villages_per_unit = 2,
-
-        terrain_defense_weight = 0.1,
-        distance_leader_weight = 0.002,
-        occupied_hex_penalty = 0.001,
-        use_max_damage_weapons = false
-    }
-
     return cfg[parm]
 end
 
