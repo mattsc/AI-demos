81947a8b0fb050893fd356ed0cbe974e9df79f57
mattsc <mattsc000@gmail.com>
2018-07-05 19:17:40 -0700

    Fred ops: move attack_utilities calculation to different file
    These are modified in the way that they return values for all functions.  That means that the following assignments won't work correctly, but there's a lot of other restructuring needed anyway.

--- AI-demos/lua/fred_benefits_utilities.lua
+++ AI-demos/lua/fred_benefits_utilities.lua
@@ -67,4 +67,123 @@ function utility_functions.retreat_utilities(move_data, value_ratio)
     return retreat_utility
 end
 
+
+function utility_functions.attack_utilities(assigned_enemies, value_ratio, fred_data)
+    local move_data = fred_data.move_data
+    local attack_utilities = {}
+    local max_rating
+    for id,_ in pairs(move_data.my_units) do
+        -- TODO: eventually put some conditional here so that we do not have
+        -- to do this for all units all the time
+        if true then
+            --std_print(id)
+            attack_utilities[id] = {}
+            for zone_id,data in pairs(assigned_enemies) do
+                --std_print('  ' .. zone_id)
+
+                local tmp_enemies = {}
+                for enemy_id,_ in pairs(data) do
+                    --std_print('    ' .. enemy_id)
+                    local att = fred_data.turn_data.unit_attacks[id][enemy_id]
+
+                    local damage_taken = att.damage_counter.enemy_gen_hc * att.damage_counter.base_taken + att.damage_counter.extra_taken
+                    damage_taken = damage_taken + att.damage_forward.enemy_gen_hc * att.damage_forward.base_taken + att.damage_forward.extra_taken
+                    damage_taken = damage_taken / 2
+
+                    local damage_done = att.damage_counter.my_gen_hc * att.damage_counter.base_done + att.damage_counter.extra_done
+                    damage_done = damage_done + att.damage_forward.my_gen_hc * att.damage_forward.base_done + att.damage_forward.extra_done
+                    damage_done = damage_done / 2
+
+                    local enemy_rating = damage_done / value_ratio - damage_taken
+                    table.insert(tmp_enemies, {
+                        damage_taken = damage_taken,
+                        damage_done = damage_done,
+                        enemy_rating = enemy_rating,
+                        enemy_id = enemy_id,
+                        my_regen = att.my_regen,
+                        enemy_regen = att.enemy_regen
+                    })
+                end
+
+                -- Only keep the 3 strongest enemies (or fewer, if there are not 3)
+                -- which means we keep the 3 with the _worst_ rating
+                table.sort(tmp_enemies, function(a, b) return a.enemy_rating < b.enemy_rating end)
+                local n = math.min(3, #tmp_enemies)
+                for i = #tmp_enemies,n+1,-1 do
+                    table.remove(tmp_enemies, i)
+                end
+
+                if (#tmp_enemies > 0) then
+                    local av_damage_taken, av_damage_done = 0, 0
+                    local cum_weight, n_enemies = 0, 0
+                    for _,enemy in pairs(tmp_enemies) do
+                        --std_print('    ' .. enemy.enemy_id)
+                        local enemy_weight = FU.unit_base_power(move_data.unit_infos[id])
+                        cum_weight = cum_weight + enemy_weight
+                        n_enemies = n_enemies + 1
+
+                        --std_print('      taken, done:', enemy.damage_taken, enemy.damage_done)
+
+                        -- For this purpose, we use individual damage, rather than combined
+                        local frac_taken = enemy.damage_taken - enemy.my_regen
+                        frac_taken = frac_taken / move_data.unit_infos[id].hitpoints
+                        --std_print('      frac_taken 1', frac_taken)
+                        frac_taken = FU.weight_s(frac_taken, 0.5)
+                        --std_print('      frac_taken 2', frac_taken)
+                        --if (frac_taken > 1) then frac_taken = 1 end
+                        --if (frac_taken < 0) then frac_taken = 0 end
+                        av_damage_taken = av_damage_taken + enemy_weight * frac_taken * move_data.unit_infos[id].hitpoints
+
+                        local frac_done = enemy.damage_done - enemy.enemy_regen
+                        frac_done = frac_done / move_data.unit_infos[enemy.enemy_id].hitpoints
+                        --std_print('      frac_done 1', frac_done)
+                        frac_done = FU.weight_s(frac_done, 0.5)
+                        --std_print('      frac_done 2', frac_done)
+                        --if (frac_done > 1) then frac_done = 1 end
+                        --if (frac_done < 0) then frac_done = 0 end
+                        av_damage_done = av_damage_done + enemy_weight * frac_done * move_data.unit_infos[enemy.enemy_id].hitpoints
+
+                        --std_print('  ', av_damage_taken, av_damage_done, cum_weight)
+                    end
+
+                    --std_print('  cum: ', av_damage_taken, av_damage_done, cum_weight)
+                    av_damage_taken = av_damage_taken / cum_weight
+                    av_damage_done = av_damage_done / cum_weight
+                    --std_print('  av:  ', av_damage_taken, av_damage_done)
+
+                    -- We want the ToD-independent rating here.
+                    -- The rounding is going to be off for ToD modifier, but good enough for now
+                    --av_damage_taken = av_damage_taken / move_data.unit_infos[id].tod_mod
+                    --av_damage_done = av_damage_done / move_data.unit_infos[id].tod_mod
+                    --std_print('  av:  ', av_damage_taken, av_damage_done)
+
+                    -- The rating must be positive for the analysis below to work
+                    local av_hp_left = move_data.unit_infos[id].hitpoints - av_damage_taken
+                    if (av_hp_left < 0) then av_hp_left = 0 end
+                    --std_print('    ' .. value_ratio, av_damage_done, av_hp_left)
+
+                    local attacker_rating = av_damage_done / value_ratio + av_hp_left
+                    attack_utilities[id][zone_id] = attacker_rating
+
+                    if (not max_rating) or (attacker_rating > max_rating) then
+                        max_rating = attacker_rating
+                    end
+                    --std_print('  -->', attacker_rating)
+                end
+            end
+        end
+    end
+    --DBG.dbms(attack_utilities)
+
+    -- Normalize the utilities
+    for id,zone_ratings in pairs(attack_utilities) do
+        for zone_id,rating in pairs(zone_ratings) do
+            zone_ratings[zone_id] = zone_ratings[zone_id] / max_rating
+        end
+    end
+    --DBG.dbms(attack_utilities)
+
+    return attack_utilities
+end
+
 return utility_functions
--- AI-demos/lua/fred_ops_utils.lua
+++ AI-demos/lua/fred_ops_utils.lua
@@ -857,153 +857,41 @@ function fred_ops_utils.set_ops_data(fred_data)
     local distance_from_front = {}
     for id,l in pairs(move_data.my_units) do
         --std_print(id, l[1], l[2])
-        if (not move_data.unit_infos[id].canrecruit) then
-            distance_from_front[id] = {}
-            for zone_id,locs in pairs(goal_hexes) do
-                --std_print('  ' .. zone_id)
-                local min_dist
-                for _,loc in ipairs(locs) do
-                    local _, cost = wesnoth.find_path(move_data.unit_copies[id], loc[1], loc[2], { ignore_units = true })
-                    cost = cost + move_data.unit_infos[id].max_moves - move_data.unit_infos[id].moves
-                    --std_print('    ' .. loc[1] .. ',' .. loc[2], cost)
+        distance_from_front[id] = {}
+        for zone_id,locs in pairs(goal_hexes) do
+            --std_print('  ' .. zone_id)
+            local min_dist
+            for _,loc in ipairs(locs) do
+                local _, cost = wesnoth.find_path(move_data.unit_copies[id], loc[1], loc[2], { ignore_units = true })
+                cost = cost + move_data.unit_infos[id].max_moves - move_data.unit_infos[id].moves
+                --std_print('    ' .. loc[1] .. ',' .. loc[2], cost)
 
-                    if (not min_dist) or (min_dist > cost) then
-                        min_dist = cost
-                    end
+                if (not min_dist) or (min_dist > cost) then
+                    min_dist = cost
                 end
-                --std_print('    -> ' .. min_dist, move_data.unit_infos[id].max_moves)
-                distance_from_front[id][zone_id] = min_dist / move_data.unit_infos[id].max_moves
             end
+            --std_print('    -> ' .. min_dist, move_data.unit_infos[id].max_moves)
+            distance_from_front[id][zone_id] = min_dist / move_data.unit_infos[id].max_moves
         end
     end
     goal_hexes = nil
     --DBG.dbms(distance_from_front)
 
 
-    local used_ids = {}
-    for zone_id,units in pairs(assigned_units) do
-        for id,_ in pairs(units) do
-            used_ids[id] = true
-        end
-    end
+    --local used_ids = {}
+    --for zone_id,units in pairs(assigned_units) do
+    --    for id,_ in pairs(units) do
+    --        used_ids[id] = true
+    --    end
+    --end
 
     local value_ratio = fred_data.turn_data.behavior.orders.value_ratio
-    local attacker_ratings = {}
-    local max_rating
-    for id,_ in pairs(move_data.my_units) do
-        if (not move_data.unit_infos[id].canrecruit)
-            and (not used_ids[id])
-        then
-            --std_print(id)
-            attacker_ratings[id] = {}
-            for zone_id,data in pairs(assigned_enemies) do
-                --std_print('  ' .. zone_id)
 
-                local tmp_enemies = {}
-                for enemy_id,_ in pairs(data) do
-                    --std_print('    ' .. enemy_id)
-                    local att = fred_data.turn_data.unit_attacks[id][enemy_id]
-
-                    local damage_taken = att.damage_counter.enemy_gen_hc * att.damage_counter.base_taken + att.damage_counter.extra_taken
-                    damage_taken = damage_taken + att.damage_forward.enemy_gen_hc * att.damage_forward.base_taken + att.damage_forward.extra_taken
-                    damage_taken = damage_taken / 2
-
-                    local damage_done = att.damage_counter.my_gen_hc * att.damage_counter.base_done + att.damage_counter.extra_done
-                    damage_done = damage_done + att.damage_forward.my_gen_hc * att.damage_forward.base_done + att.damage_forward.extra_done
-                    damage_done = damage_done / 2
-
-                    local enemy_rating = damage_done / value_ratio - damage_taken
-                    table.insert(tmp_enemies, {
-                        damage_taken = damage_taken,
-                        damage_done = damage_done,
-                        enemy_rating = enemy_rating,
-                        enemy_id = enemy_id,
-                        my_regen = att.my_regen,
-                        enemy_regen = att.enemy_regen
-                    })
-                end
-
-                -- Only keep the 3 strongest enemies (or fewer, if there are not 3)
-                -- which means we keep the 3 with the _worst_ rating
-                table.sort(tmp_enemies, function(a, b) return a.enemy_rating < b.enemy_rating end)
-                local n = math.min(3, #tmp_enemies)
-                for i = #tmp_enemies,n+1,-1 do
-                    table.remove(tmp_enemies, i)
-                end
-
-                if (#tmp_enemies > 0) then
-                    local av_damage_taken, av_damage_done = 0, 0
-                    local cum_weight, n_enemies = 0, 0
-                    for _,enemy in pairs(tmp_enemies) do
-                        --std_print('    ' .. enemy.enemy_id)
-                        local enemy_weight = FU.unit_base_power(move_data.unit_infos[id])
-                        cum_weight = cum_weight + enemy_weight
-                        n_enemies = n_enemies + 1
-
-                        --std_print('      taken, done:', enemy.damage_taken, enemy.damage_done)
-
-                        -- For this purpose, we use individual damage, rather than combined
-                        local frac_taken = enemy.damage_taken - enemy.my_regen
-                        frac_taken = frac_taken / move_data.unit_infos[id].hitpoints
-                        --std_print('      frac_taken 1', frac_taken)
-                        frac_taken = FU.weight_s(frac_taken, 0.5)
-                        --std_print('      frac_taken 2', frac_taken)
-                        --if (frac_taken > 1) then frac_taken = 1 end
-                        --if (frac_taken < 0) then frac_taken = 0 end
-                        av_damage_taken = av_damage_taken + enemy_weight * frac_taken * move_data.unit_infos[id].hitpoints
-
-                        local frac_done = enemy.damage_done - enemy.enemy_regen
-                        frac_done = frac_done / move_data.unit_infos[enemy.enemy_id].hitpoints
-                        --std_print('      frac_done 1', frac_done)
-                        frac_done = FU.weight_s(frac_done, 0.5)
-                        --std_print('      frac_done 2', frac_done)
-                        --if (frac_done > 1) then frac_done = 1 end
-                        --if (frac_done < 0) then frac_done = 0 end
-                        av_damage_done = av_damage_done + enemy_weight * frac_done * move_data.unit_infos[enemy.enemy_id].hitpoints
-
-                        --std_print('  ', av_damage_taken, av_damage_done, cum_weight)
-                    end
-
-                    --std_print('  cum: ', av_damage_taken, av_damage_done, cum_weight)
-                    av_damage_taken = av_damage_taken / cum_weight
-                    av_damage_done = av_damage_done / cum_weight
-                    --std_print('  av:  ', av_damage_taken, av_damage_done)
-
-                    -- We want the ToD-independent rating here.
-                    -- The rounding is going to be off for ToD modifier, but good enough for now
-                    --av_damage_taken = av_damage_taken / move_data.unit_infos[id].tod_mod
-                    --av_damage_done = av_damage_done / move_data.unit_infos[id].tod_mod
-                    --std_print('  av:  ', av_damage_taken, av_damage_done)
-
-                    -- The rating must be positive for the analysis below to work
-                    local av_hp_left = move_data.unit_infos[id].hitpoints - av_damage_taken
-                    if (av_hp_left < 0) then av_hp_left = 0 end
-                    --std_print('    ' .. value_ratio, av_damage_done, av_hp_left)
-
-                    local attacker_rating = av_damage_done / value_ratio + av_hp_left
-                    attacker_ratings[id][zone_id] = attacker_rating
-
-                    if (not max_rating) or (attacker_rating > max_rating) then
-                        max_rating = attacker_rating
-                    end
-                    --std_print('  -->', attacker_rating)
-                end
-            end
-        end
-    end
-    --DBG.dbms(attacker_ratings)
-
-
-    -- Normalize the ratings
-    for id,zone_ratings in pairs(attacker_ratings) do
-        for zone_id,rating in pairs(zone_ratings) do
-            zone_ratings[zone_id] = zone_ratings[zone_id] / max_rating
-        end
-    end
-    --DBG.dbms(attacker_ratings)
+    local attack_utilities = FBU.attack_utilities(assigned_enemies, value_ratio, fred_data)
+    --DBG.dbms(attack_utilities)
 
     local unit_ratings = {}
-    for id,zone_ratings in pairs(attacker_ratings) do
+    for id,zone_ratings in pairs(attack_utilities) do
         --std_print(id)
         unit_ratings[id] = {}
 
@@ -1175,7 +1063,7 @@ function fred_ops_utils.set_ops_data(fred_data)
     for id,_ in pairs(unit_ratings) do
         reserve_units[id] = true
     end
-    attacker_ratings = nil
+    attack_utilities = nil
     unit_ratings = nil
 
     zone_power_stats = fred_ops_utils.zone_power_stats(raw_cfgs_main, assigned_units, assigned_enemies, fred_data)
