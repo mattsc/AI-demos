a81305daa301474d236b899a5e96ef416b3881b1
mattsc <mattsc000@gmail.com>
2019-02-01 07:11:28 -0800

    Fred virtual state library: enable nested uses of FVS functions

--- AI-demos/lua/fred_virtual_state.lua
+++ AI-demos/lua/fred_virtual_state.lua
@@ -11,7 +11,7 @@ local FVS_stored_units, FVS_ids, FVS_add_units
 
 
 ----- Begin adjust_move_data_tables() -----
-local function adjust_move_data_tables(old_locs, new_locs, store_units_in_way, move_data)
+local function adjust_move_data_tables(old_locs, new_locs, store_units_in_way, move_data, stored_data)
     -- Adjust all the move_data tables to the new position, and reset them again later.
     -- This is a local function as only the virtual state calculations should have to move units.
     --
@@ -20,6 +20,10 @@ local function adjust_move_data_tables(old_locs, new_locs, store_units_in_way, m
     -- @store_units_in_way (boolean): whether to store the locations of the units in the way. Needs to
     -- be set to 'true' when moving the units into their new locations, needs to be set to
     -- 'false' when moving them back, in which case the stored information will be used.
+    --
+    -- OPTIONAL INPUTS:
+    -- @stored_data: table for temporary storage of units and ids. This is usually done automatically
+    --   in the FVS_* variables via closure, but that does not work with nested uses of these functions.
     --
     -- Output:
     -- Returns nil if no counter attacks were found, otherwise a table with a
@@ -39,7 +43,11 @@ local function adjust_move_data_tables(old_locs, new_locs, store_units_in_way, m
             -- but not units without MP (as the latter are already on the map)
             -- Note: this array might have missing elements -> needs to be iterated using pairs()
             if move_data.my_unit_map_MP[x1] and move_data.my_unit_map_MP[x1][y1] then
-                FVS_ids[i_l] = move_data.my_unit_map_MP[x1][y1].id
+                if stored_data then
+                    stored_data.FVS_ids[i_l] = move_data.my_unit_map_MP[x1][y1].id
+                else
+                    FVS_ids[i_l] = move_data.my_unit_map_MP[x1][y1].id
+                end
             end
 
             -- By contrast, we only need to store the information about units in the way,
@@ -48,7 +56,11 @@ local function adjust_move_data_tables(old_locs, new_locs, store_units_in_way, m
                 -- If there is another unit at the new location, store it
                 -- It does not matter for this whether this is a unit involved in the move or not
                 if move_data.my_unit_map[x2] and move_data.my_unit_map[x2][y2] then
-                    FVS_stored_units[move_data.my_unit_map[x2][y2].id] = { x2, y2 }
+                    if stored_data then
+                        stored_data.FVS_stored_units[move_data.my_unit_map[x2][y2].id] = { x2, y2 }
+                    else
+                        FVS_stored_units[move_data.my_unit_map[x2][y2].id] = { x2, y2 }
+                    end
                 end
             end
         end
@@ -62,7 +74,7 @@ local function adjust_move_data_tables(old_locs, new_locs, store_units_in_way, m
 
         -- We only need to do this if the unit actually gets moved
         if (x1 ~= x2) or (y1 ~= y2) then
-            local id = FVS_ids[i_l]
+            local id = stored_data and stored_data.FVS_ids[i_l] or FVS_ids[i_l]
 
             -- Likely, not all of these tables have to be changed, but it takes
             -- very little time, so better safe than sorry
@@ -90,7 +102,7 @@ local function adjust_move_data_tables(old_locs, new_locs, store_units_in_way, m
     -- Finally, if 'store_units_in_way' is not set (this is, when moving units back
     -- into place), restore the stored units into the maps again
     if (not store_units_in_way) then
-        for id,loc in pairs(FVS_stored_units) do
+        for id,loc in pairs(stored_data and stored_data.FVS_stored_units or FVS_stored_units) do
             move_data.my_unit_map[loc[1]][loc[2]] = { id = id }
             move_data.my_unit_map_MP[loc[1]][loc[2]] = { id = id }
         end
@@ -101,15 +113,19 @@ end
 
 local fred_virtual_state = {}
 
-function fred_virtual_state.set_virtual_state(old_locs, new_locs, additional_units, move_data)
-    FVS_stored_units, FVS_ids, FVS_add_units = {}, {}, {}
+function fred_virtual_state.set_virtual_state(old_locs, new_locs, additional_units, move_data, stored_data)
+    if stored_data then
+        stored_data.FVS_stored_units, stored_data.FVS_ids, stored_data.FVS_add_units = {}, {}, {}
+    else
+        FVS_stored_units, FVS_ids, FVS_add_units = {}, {}, {}
+    end
 
     -- Mark the new positions of the units in the move_data tables
-    adjust_move_data_tables(old_locs, new_locs, true, move_data)
+    adjust_move_data_tables(old_locs, new_locs, true, move_data, stored_data)
 
     -- Put all units in old_locs with MP onto the map (those without are already there)
     -- They need to be proxy units for the counter attack calculation.
-    for _,id in pairs(FVS_ids) do
+    for _,id in pairs(stored_data and stored_data.FVS_ids or FVS_ids) do
         wesnoth.put_unit(move_data.unit_copies[id])
     end
 
@@ -124,7 +140,11 @@ function fred_virtual_state.set_virtual_state(old_locs, new_locs, additional_uni
                 end
             end
             if place_unit then
-                table.insert(FVS_add_units, add_unit)
+                if stored_data then
+                    table.insert(stored_data.FVS_add_units, add_unit)
+                else
+                    table.insert(FVS_add_units, add_unit)
+                end
 
                 wesnoth.put_unit({
                     type = add_unit.type,
@@ -140,18 +160,22 @@ function fred_virtual_state.set_virtual_state(old_locs, new_locs, additional_uni
     end
 end
 
-function fred_virtual_state.reset_state(old_locs, new_locs, move_data)
+function fred_virtual_state.reset_state(old_locs, new_locs, move_data, stored_data)
     -- Extract the units from the map
-    for _,add_unit in ipairs(FVS_add_units) do
+    for _,add_unit in ipairs(stored_data and stored_data.FVS_add_units or FVS_add_units) do
         wesnoth.erase_unit(add_unit[1], add_unit[2])
     end
-    for _,id in pairs(FVS_ids) do
+    for _,id in pairs(stored_data and stored_data.FVS_ids or FVS_ids) do
         wesnoth.extract_unit(move_data.unit_copies[id])
     end
 
     -- And put them back into their original locations
-    adjust_move_data_tables(new_locs, old_locs, false, move_data)
-    FVS_stored_units, FVS_ids, FVS_add_units = nil, nil, nil
+    adjust_move_data_tables(new_locs, old_locs, false, move_data, stored_data)
+    if stored_data then
+        stored_data.FVS_stored_units, stored_data.FVS_ids, stored_data.FVS_add_units = nil, nil, nil
+    else
+        FVS_stored_units, FVS_ids, FVS_add_units = nil, nil, nil
+    end
 end
 
 function fred_virtual_state.virtual_reach_maps(units, to_enemy_locs, to_locs, move_data)
