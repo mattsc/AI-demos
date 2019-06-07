eca87af9f41672a01e11f2a00c91dd151ee29477
mattsc <mattsc000@gmail.com>
2019-06-07 06:40:22 -0700

    Fred holding: get raw_cfg from map_config function
    ... rather than from ops_data. That's because the leader zone, if used, is not stored in ops_data. All of this will have to be cleaned up at some point.

--- AI-demos/lua/ca_zone_control.lua
+++ AI-demos/lua/ca_zone_control.lua
@@ -1428,7 +1428,7 @@ local function get_hold_action(zone_cfg, fred_data)
     --std_print('protect_forward_rating_weight', protect_forward_rating_weight)
 
 
-    local raw_cfg = fred_data.ops_data.raw_cfgs[zone_cfg.zone_id]
+    local raw_cfg = FMC.get_raw_cfgs(zone_cfg.zone_id)
     --DBG.dbms(raw_cfg, false, 'raw_cfg')
     --DBG.dbms(zone_cfg, false, 'zone_cfg')
 
