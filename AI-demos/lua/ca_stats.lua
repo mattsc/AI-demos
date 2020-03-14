4e4b7d2c92de0ad4d2ff9af8cb90f7c5971079bb
mattsc <mattsc000@gmail.com>
2020-03-13 18:54:31 -0700

    Code cleanup: make ca_stats.lua independent of move_data
    This is called exactly once per turn and the two wesnoth.* calls are not expensive.  There's really no need for this to depend on other CAs.

--- AI-demos/lua/ca_stats.lua
+++ AI-demos/lua/ca_stats.lua
@@ -2,7 +2,7 @@
 -- This will be blacklisted after first execution each turn
 
 local DBG = wesnoth.dofile "~/add-ons/AI-demos/lua/debug.lua"
-local FGM = wesnoth.require "~/add-ons/AI-demos/lua/fred_gamestate_map.lua"
+local COMP = wesnoth.require "~/add-ons/AI-demos/lua/compatibility.lua"
 
 local ca_stats = {}
 
@@ -10,37 +10,37 @@ function ca_stats:evaluation()
     return 999990
 end
 
-function ca_stats:execution(cfg, data)
+function ca_stats:execution()
     local tod = wesnoth.get_time_of_day()
     std_print('\n**** Fred Side ' .. wesnoth.current.side .. ' (version ' .. wesnoth.dofile('~/add-ons/AI-demos/version.lua') .. ') *******************************************************')
     DBG.print_ts('Beginning of Turn ' .. wesnoth.current.turn .. ' (' .. tod.name ..') stats')
 
     local sides = {}
     local leveled_units
-    for id,_ in pairs(data.move_data.units) do
-        local unit_side = data.move_data.unit_infos[id].side
+    for _,unit_proxy in ipairs(COMP.get_units()) do
+        local unit_side = unit_proxy.side
         if (not sides[unit_side]) then sides[unit_side] = {} end
 
         sides[unit_side].num_units = (sides[unit_side].num_units or 0) + 1
-        sides[unit_side].hitpoints = (sides[unit_side].hitpoints or 0) + data.move_data.unit_infos[id].hitpoints
+        sides[unit_side].hitpoints = (sides[unit_side].hitpoints or 0) + unit_proxy.hitpoints
 
-        if data.move_data.unit_infos[id].canrecruit then
-            sides[unit_side].leader_type = data.move_data.unit_copies[id].type
-            sides[unit_side].leader_hp = data.move_data.unit_infos[id].hitpoints
-            sides[unit_side].leader_max_hp = data.move_data.unit_infos[id].max_hitpoints
+        if unit_proxy.canrecruit then
+            sides[unit_side].leader_type = unit_proxy.type
+            sides[unit_side].leader_hp = unit_proxy.hitpoints
+            sides[unit_side].leader_max_hp = unit_proxy.max_hitpoints
         else
-            if (unit_side == wesnoth.current.side) and (data.move_data.unit_infos[id].level > 1) then
+            if (unit_side == wesnoth.current.side) and (unit_proxy.level > 1) then
                 if (not leveled_units) then leveled_units = '' end
                 leveled_units = leveled_units
-                    .. data.move_data.unit_infos[id].type .. ' ('
-                    .. data.move_data.unit_infos[id].hitpoints .. '/' .. data.move_data.unit_infos[id].max_hitpoints .. ')  '
+                    .. unit_proxy.type .. ' ('
+                    .. unit_proxy.hitpoints .. '/' .. unit_proxy.max_hitpoints .. ')  '
             end
         end
     end
 
     local total_villages = 0
-    for x,y,village in FGM.iter(data.move_data.village_map) do
-        local owner = village.owner
+    for _,village in ipairs(wesnoth.get_villages()) do
+        local owner = wesnoth.get_village_owner(village[1], village[2]) or 0
         if (owner > 0) then
             sides[owner].num_villages = (sides[owner].num_villages or 0) + 1
         end
